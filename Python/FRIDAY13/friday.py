a42218b3772828916725f6871d388d2145505b57
Joseph Silva <119358195+JosephSC0121@users.noreply.github.com>
2023-04-01 23:23:42 -0500

    Update friday.py
    Good practices

--- Python/FRIDAY13/friday.py
+++ Python/FRIDAY13/friday.py
@@ -1,15 +1,18 @@
 import calendar
 
-object = calendar.Calendar()
+objCal = calendar.Calendar()
 
 def findFriday():
-    for day in object.itermonthdays3(year, month):
-        if day[2]==13 and calendar.weekday(*day):
+    for day in objectCal.itermonthdays3(year, month):
+        if day[2]==13 and calendar.weekday(*day)==4:
             return True
-        return False
+    return False
 
 if __name__ == "__main__":
     month = int(input("Type the number of a month: "))
     year = int(input("Type a year: "))
-    print(findFriday())
+    if findFriday():
+        print("The 13th of the month is a Friday!")
+    else:
+        print("The 13th of the month is not a Friday.")
 
