bd5fd9e28c811571b086b9bfd45c09b083f6a352
Joseph Silva <119358195+JosephSC0121@users.noreply.github.com>
2023-04-20 18:44:24 -0500

    Update problems

---
+++ Euler-Proyect/Euler_2.py
@@ -0,0 +1,24 @@
+import math
+
+def is_fibonacci(n):
+    # Check if the input value is valid
+    if n <= 0:
+        return False
+    # Check if the number is a perfect square
+    root1 = math.isqrt(5*n*n + 4)
+    root2 = math.isqrt(5*n*n - 4)
+    if root1**2 == 5*n*n + 4 or root2**2 == 5*n*n - 4:
+        return True
+    else:
+        return False
+
+def comparation():
+    add = 0
+    for i in range(1,4000000):
+        if is_fibonacci(i)==True and i%2==0:
+            add += i
+    return add
+
+if __name__ == "__main__":
+    print(comparation())
+
